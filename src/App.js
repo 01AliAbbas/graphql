45e0dcddede85091db00d65274317294b85a0985
aliabbas0 <ali.am.se21@gmail.com>
2025-02-12 21:16:26 +0300

    route bug

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Login from "./components/Login";
 import Profile from "./components/Profile";
-import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { ThemeProvider } from "@mui/material/styles";
 import quantumTheme from './theme';
 
 function App() {
     return (
         <ThemeProvider theme={quantumTheme}>
-            <Router>
+            <Router basename="/graphql">
                 <Routes>
                     <Route path="/" element={<Login />} />
                     <Route path="/profile" element={<Profile />} />
+                    {/* Add catch-all route */}
+                    <Route path="*" element={<Navigate to="/" replace />} />
                 </Routes>
             </Router>
         </ThemeProvider>
